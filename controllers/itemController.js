97e55837f3efb3bcdef0f22f6e60bb47d4d63de3
KalpaniKapuge <kalpanikapuge1020@gmail.com>
2025-08-20 19:33:15 +0530

    Implement item controller✏️

---
+++ controllers/itemController.js
@@ -0,0 +1,59 @@
+import shortid from 'shortid';
+import Item from '../models/Item.js';
+
+exports.createItem = async (req, res) => {
+  const { code, price, quantity } = req.body;
+  try {
+    const item = new Item({
+      id: shortid.generate(),
+      code,
+      price,
+      quantity,
+    });
+    await item.save();
+    res.json(item);
+  } catch (err) {
+    res.status(500).json({ 
+        msg: err.message 
+    });
+  }
+};
+
+exports.getItems = async (req, res) => {
+  try {
+    const items = await Item.find();
+    res.json(items);
+  } catch (err) {
+    res.status(500).json({ 
+        msg: err.message 
+    });
+  }
+};
+
+exports.updateItem = async (req, res) => {
+  try {
+    const item = await Item.findOneAndUpdate({ id: req.params.id }, req.body, { new: true });
+    if (!item) return res.status(404).json({ msg: 'Not found' });
+    res.json(item);
+  } catch (err) {
+    res.status(500).json({ 
+        msg: err.message 
+    });
+  }
+};
+
+exports.deleteItem = async (req, res) => {
+  try {
+    const item = await Item.findOneAndDelete({ id: req.params.id });
+    if (!item) return res.status(404).json({ 
+        msg: 'Not found' 
+    });
+    res.json({ 
+        msg: 'Deleted' 
+    });
+  } catch (err) {
+    res.status(500).json({ 
+        msg: err.message 
+    });
+  }
+};
