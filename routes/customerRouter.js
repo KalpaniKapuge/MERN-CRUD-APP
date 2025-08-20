c9ab0b494f90cfe759e474917d91c2fb66a25d4c
KalpaniKapuge <kalpanikapuge1020@gmail.com>
2025-08-20 19:47:17 +0530

    Implement auth middleware✏️

---
+++ routes/customerRouter.js
@@ -0,0 +1,11 @@
+// const express = require('express');
+// const { createCustomer, getCustomers, updateCustomer, deleteCustomer } = require('../controllers/customerController');
+// const auth = require('../middleware/auth');
+// const router = express.Router();
+
+// router.post('/', auth, createCustomer);
+// router.get('/', auth, getCustomers);
+// router.put('/:id', auth, updateCustomer);
+// router.delete('/:id', auth, deleteCustomer);
+
+// module.exports = router;
