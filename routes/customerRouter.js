c956cdfd8be41eded717b8b83b2c0446ef419672
KalpaniKapuge <kalpanikapuge1020@gmail.com>
2025-08-20 19:51:58 +0530

    Implement customer router✏️

--- routes/customerRouter.js
+++ routes/customerRouter.js
@@ -1,11 +1,12 @@
-// const express = require('express');
-// const { createCustomer, getCustomers, updateCustomer, deleteCustomer } = require('../controllers/customerController');
-// const auth = require('../middleware/auth');
-// const router = express.Router();
+import express from 'express';
+import { createCustomer, getCustomers, updateCustomer, deleteCustomer } from '../controllers/customerController.js';
+import auth from '../middleware/auth';
 
-// router.post('/', auth, createCustomer);
-// router.get('/', auth, getCustomers);
-// router.put('/:id', auth, updateCustomer);
-// router.delete('/:id', auth, deleteCustomer);
+const customerRouter = express.Router();
 
-// module.exports = router;
+router.post('/', auth, createCustomer);
+router.get('/', auth, getCustomers);
+router.put('/:id', auth, updateCustomer);
+router.delete('/:id', auth, deleteCustomer);
+
+export default customerRouter;
